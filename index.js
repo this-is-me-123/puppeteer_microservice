0ff3832148f8a88813592f21f4553f6126e40cc9
this-is-me-123 <my.personal.email.cv@gmail.com>
2025-05-22 08:37:02 -0400

    Update index.js

--- index.js
+++ index.js
@@ -1,13 +1,13 @@
 const puppeteer = require('puppeteer');
 
 (async () => {
-try {
-const browser = await puppeteer.launch({ headless: true, args: ['--no-sandbox'] });
-const version = await browser.version();
-console.log(✅ Chromium is working: ${version});
-await browser.close();
-} catch (err) {
-console.error('❌ Chromium launch failed:', err);
-process.exit(1);
-}
+  try {
+    const browser = await puppeteer.launch({ headless: true, args: ['--no-sandbox'] });
+    const version = await browser.version();
+    console.log(`✅ Chromium is working: ${version}`);
+    await browser.close();
+  } catch (err) {
+    console.error('❌ Chromium launch failed:', err);
+    process.exit(1);
+  }
 })();
