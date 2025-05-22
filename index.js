a162b93a4eb892550b06bbe2d7144e00224c63dc
this-is-me-123 <my.personal.email.cv@gmail.com>
2025-05-22 08:11:25 -0400

    Update index.js

--- index.js
+++ index.js
@@ -11,19 +11,3 @@ console.error('❌ Chromium launch failed:', err);
 process.exit(1);
 }
 })();
-
-const express = require('express');
-const dotenv = require('dotenv');
-dotenv.config();
-
-const app = express();
-const PORT = process.env.PORT || 3000;
-
-const loginRoute = require('./routes/login');
-
-app.use(express.json());
-app.use('/login', loginRoute);
-
-app.listen(PORT, () => {
-console.log(🚀 Server running on port ${PORT});
-});
